28978c35d07bf0ee2f7a4c71d9a3d4546c95e099
Diana <dianam0@rambler.ru>
2023-03-05 23:22:51 +0300

    added third task

--- src/main/java/clrawler/Tokenizer.java
+++ src/main/java/clrawler/Tokenizer.java
@@ -2,7 +2,10 @@ package clrawler;
 
 import org.jsoup.Jsoup;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,18 +15,23 @@ public class Tokenizer {
         return Jsoup.parse(htmlContent).text();
     }
 
-    public void retrieveTokens() {
+    public void retrieveTokens(boolean writeIntoSingleFile) {
         File directory = new File("sites");
         File[] files = directory.listFiles();
         if (files == null) return;
+        Integer i = writeIntoSingleFile ? null : 1;
         Set<String> tokens = new HashSet<>();
         for (File file : files) {
-            retrieveTokens(tokens, file);
+            retrieveTokens(tokens, file, writeIntoSingleFile, i);
+            if (!writeIntoSingleFile) {
+                File tokenFile = new File(Utils.generateFileName(Utils.WORDS_TOKEN_DIR, i));
+                i++;
+            }
         }
         Utils.writeToFileLineByLine(new File(Utils.TOKENS_FILE_PATH), new ArrayList<>(tokens), true);
     }
 
-    private void retrieveTokens(Set<String> tokens, File source) {
+    private void retrieveTokens(Set<String> tokens, File source, boolean needToWriteInSingleFile, Integer index) {
         StringBuilder sb = new StringBuilder();
         Set<String> retrievedTokens = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader(source))) {
@@ -55,6 +63,38 @@ public class Tokenizer {
                 .filter(token -> !token.toLowerCase().matches(Utils.UNIONS_REGEX_PATTERN))
                 .filter(token -> !token.toLowerCase().matches(Utils.PREPOSITIONS_REGEX_PATTERN))
                 .collect(Collectors.toSet()));
+
+        if (needToWriteInSingleFile) {
+            Utils.writeToFileLineByLine(
+                    new File(Utils.TOKENS_FILE_PATH),
+                    new LinkedList<>(tokens),
+                    false
+            );
+        } else {
+            Utils.writeToFileLineByLine(
+                    new File(Utils.generateFileName(Utils.WORDS_TOKEN_DIR, index)),
+                    new LinkedList<>(tokens),
+                    false
+            );
+        }
+//        removeDuplicates(source, true);
     }
 
+    private void removeDuplicates(File source, boolean shouldDelete) {
+        Set<String> noDuplicatesSet = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                noDuplicatesSet.add(line.toLowerCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Utils.writeToFileLineByLine(new File(Utils.TOKENS_FILE_PATH), new LinkedList<>(noDuplicatesSet), shouldDelete);
+        Utils.writeToFileLineByLine(
+                source,
+                new ArrayList<>(noDuplicatesSet).stream().sorted().collect(Collectors.toList()),
+                shouldDelete
+        );
+    }
 }
